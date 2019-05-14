fe97c04a81fe7fe4d9f6d4e8d4757d6c77abc167
Hyeongshin Yoon <tkxkd7223@gamil.com>
2019-05-14 11:10:43 +0900

    change timer in shared page

--- static/js/shared_page.js
+++ static/js/shared_page.js
@@ -1,60 +1,77 @@
-/* ===== progress bar variables ===== */
-$(function() {
-  var current_progress = 0;
-  var interval = setInterval(function() {
-    current_progress += 10;
-    $("#dynamic")
-    .css("width", current_progress + "%")
-    .attr("aria-valuenow", current_progress)
-    .text(current_progress + "% Complete");
-    if (current_progress >= 100)
-      clearInterval(interval);
-  }, 1000);
-});
-
-/* ===== countdown timer javascript ===== */
-let target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
-let days, hours, minutes, seconds; // variables for time units
-
-getCountdown();
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-  console.log("counting..");
-  // find the amount of "seconds" between now and target
-  let current_date = new Date().getTime();
-  let seconds_left = (target_date - current_date) / 1000;
-
-  days = pad( parseInt(seconds_left / 86400) );
-  seconds_left = seconds_left % 86400;
-  hours = pad( parseInt(seconds_left / 3600) );
-  seconds_left = seconds_left % 3600;
-  minutes = pad( parseInt(seconds_left / 60) );
-  seconds = pad( parseInt( seconds_left % 60 ) );
-
-  // format countdown string + set tag value
-  document.getElementById("tiles").innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
-}
-
-function pad(n) {
-  return (n < 10 ? '0' : '') + n;
-}
-
-
 new Vue({
   el: "#sharedPageBodyWrapper",
   name: "shared_page_vue",
   data() {
     return {
-      username: "HyoungJo",
-      end_time: "May 21th, 2019 18:00",
-      user_goals: [],
+      username: "HyoungJo", // It is empty at first.
+      time_left: "", // It is empty at first.
+      print_end_time: "", // It is empty at first.
+      start_time: new Date("2019-05-10T09:00:00"), // It is empty at first.
+      end_time: new Date("2019-06-01T11:00:00"), // It is empty at first.
+      complete_percentage: 0,
+      user_goals: ["0.1 iPad", "12 Americanos", "0.01 Paris Trip"], // It is empty at first.
     }
   },
   methods: {
 
   },
-  created() {
-    console.log("shared_page js loaded")
-  }
+  mounted() {
+    // this.getUser();
+    this.print_end_time = this.getTimeStamp(this.end_time);
+    // Update the count down every 1 second
+    this.timerCount(this.start_time,this.end_time);
+    this.interval = setInterval(() => {
+        this.timerCount(this.start,this.end);
+    }, 1000);
+  },
+  methods: {
+    getTimeStamp: function(date) {
+      var s =
+        this.leadingZeros(date.getFullYear(), 4) + '-' +
+        this.leadingZeros(date.getMonth() + 1, 2) + '-' +
+        this.leadingZeros(date.getDate(), 2) + ' ' +
+
+        this.leadingZeros(date.getHours(), 2) + ':' +
+        this.leadingZeros(date.getMinutes(), 2) + ':' +
+        this.leadingZeros(date.getSeconds(), 2);
+
+      return s;
+    },
+    leadingZeros: function(n, digits) {
+      var zero = '';
+      n = n.toString();
+      for (i = 0; i < digits - n.length; i++)
+        zero += '0';
+      return zero + n;
+    },
+    timerCount: function(start,end){
+        // Get todays date and time
+        var now = new Date();
+
+        // Find the distance between now an the count down date
+        this.time_left = this.end_time - now;
+        this.getCountdown();
+        var percent = (now - this.start_time) / (this.end_time - this.start_time) * 100;
+        this.complete_percentage = percent.toFixed(4);
+        $("#dynamic")
+        .css("width", this.complete_percentage + "%")
+        .attr("aria-valuenow", this.complete_percentage)
+        .text(this.complete_percentage + "% Complete");
+    },
+    getCountdown: function(){
+      // find the amount of "seconds" between now and target
+      var current_date = new Date();
+      var seconds_left = (this.end_time - current_date) / 1000;
+
+      var days = this.leadingZeros( parseInt(seconds_left / 86400) );
+      seconds_left = seconds_left % 86400;
+      var hours = this.leadingZeros( parseInt(seconds_left / 3600) );
+      seconds_left = seconds_left % 3600;
+      var minutes = this.leadingZeros( parseInt(seconds_left / 60) );
+      var seconds = this.leadingZeros( parseInt( seconds_left % 60 ) );
+
+      // format countdown string + set tag value
+      document.getElementById("tiles").innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+    }
+  },
 });

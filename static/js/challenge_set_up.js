88bd978900d018f8ca68c3fad81c1e59151949c9
call-me-soo <k.callmesoo@gmail.com>
2019-05-27 04:12:35 +0900

    0527

--- static/js/challenge_set_up.js
+++ static/js/challenge_set_up.js
@@ -2,23 +2,42 @@ new Vue({
     el: '#challenge_set_up',
     data: {
         user: {},
+        mode: '',
         dependency: 'alcohol',
         spent_money: 10000,
-        spent_per: 'day',
-
+        spent_per: 'day'
     },
     mounted: function (){
+        this.getImage();
+        this.mode = mode;
+        if (mode === 'create'){
+            this.user = user_info;
 
-        // this.$http.get('user_info').then(response => {
-        //     this.user = response.data;
-        // })
+        } else {
+            this.getUser().then(function(response){
+                this.user = response.data;
+            })
+        }
 
-    },
-    computed: {
     },
     methods: {
-        getImage: function(){
-            dependencyImage: '../img/alcohol.png'
+        getImage: function() {
+            $('#dependency_image').attr('src', '../static/img/' + this.dependency + '.png')
+        },
+        getUser: function() {
+            this.$http.post('/json/user/' + this.user['id'])
+        },
+        createUser: function () {
+            this.$http.post('/create_user', this.user)
+                .then(
+                    alert('user is created')
+                )
+        },
+        updateUser: function () {
+            this.$http.post('/update_user', this.user)
+                .then(
+                    alert('user information is updated')
+                )
         }
 
     }

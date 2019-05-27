aceda64a28c6f4b6883dcc52455e304f1098ce5d
Hyeongshin Yoon <tkxkd7223@gamil.com>
2019-05-27 09:42:24 +0900

    make dynamic table

--- static/js/shared_page.js
+++ static/js/shared_page.js
@@ -1,32 +1,51 @@
 new Vue({
   el: "#sharedPageBodyWrapper",
   name: "shared_page_vue",
-  data() {
-    return {
-      userid: "HyoungJo", // It is empty at first.
-      time_left: "", // It is empty at first.
-      print_end_time: "", // It is empty at first.
-      start_time: new Date("2019-05-10T09:00:00"), // It is empty at first.
-      end_time: new Date("2019-06-01T11:00:00"), // It is empty at first.
-      complete_percentage: 0,
-      user_goals: ["0.1 iPad", "12 Americanos", "0.01 Paris Trip"], // It is empty at first.
-      spent_money: 10000, // It is empty at first.
-      spent_per: "Day", // It is empty at first.
-      spent_money_ms: 0, // It is empty at first.
-      saved_money: "", // It is empty at first.
-      image: '',
-      report_text: "",
-      reports: [],
-      cheeringMessage: '',
-      cheeringFriendName: '',
-      cheeringMessageList: [],
-      pokingFriendName: '',
-      pokingList: [],
-      bettingSide: '',
-      bettingFriendName: '',
-      bettingAmount: '',
-      bettingList: [],
-    }
+  data: {
+    user: {},
+    userid: "HyoungJo", // It is empty at first.
+    time_left: "", // It is empty at first.
+    print_end_time: "", // It is empty at first.
+    start_time: new Date("2019-05-10T09:00:00"), // It is empty at first.
+    end_time: new Date("2019-06-01T11:00:00"), // It is empty at first.
+    complete_percentage: 0,
+    user_goals: [],
+    goals: [
+      {'name': 'iPad', 'money': 1000000},
+      {'name': 'Americano', 'money': 5000},
+      {'name': 'Paris Trip', 'money': 4000000},
+    ], // It is empty at first.
+    spent_money: 10000, // It is empty at first.
+    spent_per: "Day", // It is empty at first.
+    spent_money_ms: 0, // It is empty at first.
+    saved_money: "", // It is empty at first.
+    print_saved_money: "",
+    image: '',
+    report_text: "",
+    reports: [],
+    cheeringMessage: '',
+    cheeringFriendName: '',
+    cheeringMessageList: [],
+    pokingFriendName: '',
+    pokingList: [
+      {'pokingFriendName': 'Jason', 'numberOfPoking': 34},
+      {'pokingFriendName': 'Christina', 'numberOfPoking': 26},
+      {'pokingFriendName': 'Hyeongshin', 'numberOfPoking': 20},
+      {'pokingFriendName': 'Jinwon', 'numberOfPoking': 14},
+      {'pokingFriendName': 'Suyeon', 'numberOfPoking': 9},
+    ],
+    bettingSide: '',
+    bettingFriendName: '',
+    bettingAmount: '',
+    succeedBettingList: [
+      {'bettingAmount': 20000, 'bettingFriendName': 'Simon'},
+      {'bettingAmount': 15000, 'bettingFriendName': 'James'},
+    ],
+    failureBettingList: [
+      {'bettingAmount': 10000, 'bettingFriendName': 'Simon'},
+      {'bettingAmount': 26000, 'bettingFriendName': 'James'},
+      {'bettingAmount': 21000, 'bettingFriendName': 'James'},
+    ],
   },
   created() {
     if(this.spent_per == "Day")
@@ -37,7 +56,9 @@ new Vue({
       this.spent_money_ms = this.spent_money / (86400000 * 7 * 30);
   },
   mounted() {
-    // this.getUser();
+    this.pokeView();
+    this.betView();
+    this.goalView();
     this.print_end_time = this.getTimeStamp(this.end_time);
     // Update the count down every 1 second
     this.timerCount();
@@ -46,6 +67,23 @@ new Vue({
     }, 1000);
   },
   methods: {
+    getUser: function() {
+      this.$http.post('/json/user/' + this.user['id'])
+      // bring name and change username variable
+      this.userid = this.user["nickname"];
+      var now = new Date().getTime();
+      this.time_left = this.user["goal_date"] - now;
+      this.end_time = this.user["goal_date"];
+      var percent = this.time_left / (this.user["goal_date"] - this.user["start_date"]);
+      this.complete_percentage = percent * 100;
+      this.user_goals = [this.user["mini_goal_1"], this.user["mini_goal_2"], this.user["mini_goal_3"]];
+    },
+    updateUser: function () {
+      this.$http.post('/update_user', this.user)
+      .then(
+        alert('user information is updated')
+      )
+    },
     getTimeStamp: function(date) {
       var s =
         this.leadingZeros(date.getFullYear(), 4) + '-' +
@@ -67,7 +105,8 @@ new Vue({
     },
     moneyCount: function(start,now){
       var calc_saved_money = (now - start) * this.spent_money_ms;
-      this.saved_money = this.numberWithCommas(calc_saved_money.toFixed(2));
+      this.saved_money = calc_saved_money.toFixed(2);
+      this.print_saved_money = this.numberWithCommas(this.saved_money);
     },
     timerCount: function(){
         // Get todays date and time
@@ -77,16 +116,14 @@ new Vue({
         // Find the distance between now an the count down date
         this.time_left = end - now;
         this.getCountdown();
+        this.goalView();
         this.moneyCount(start,now);
         var percent = (now - start) / (end - start) * 100;
-        this.complete_percentage = percent.toFixed(4);
+        this.complete_percentage = percent.toFixed(2);
         $("#dynamic")
         .css("width", this.complete_percentage + "%")
         .attr("aria-valuenow", this.complete_percentage)
         .text(this.complete_percentage + "% Complete");
-    },
-    numberWithCommas: function(x){
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
     getCountdown: function(){
       // find the amount of "seconds" between now and target
@@ -139,37 +176,102 @@ new Vue({
       }
     },
     sendCheeringMessage: function(e) {
-      if (this.cheeringMessage is null) {
+      if (!this.cheeringMessage) {
         alert("Please fill in the message");
       }
-      if (this.cheeringFriendName is null) {
+      if (!this.cheeringFriendName) {
         this.cheeringFriendName = 'friendNoName';
       }
       this.cheeringMessageList.push({'cheeringMessage': this.cheeringMessage, 'cheeringFriendName': this.cheeringFriendName});
     },
+    pokeView: function() {
+        var poke = $('#pokeTable');
+        $("#pokeTable").nextAll().remove();
+        if (this.pokingList){
+          for (var i = this.pokingList.length - 1 ; i >= 0 ; i--) {
+            var newRow = $("<tr></tr>");
+            var newIndex = $(`<th scope="row">${i+1}</th>`);
+            var newNumber = $(`<td>${this.pokingList[i]['numberOfPoking']}</td>`);
+            var newName = $(`<td>${this.pokingList[i]['pokingFriendName']}</td>`);
+            newRow.append([newIndex, newNumber, newName]);
+            newRow.insertAfter(poke);
+          }
+        }
+    },
     onPoking: function(e) {
-      if (this.pokingFriendName is null) {
+      if (!this.pokingFriendName) {
         alert("Please fill in your name!");
         return;
       }
-      if (this.pokingList includes this.pokingFriendName) {
-        this.pokingList[this.pokingFriendName] += 1;
+      var index = this.pokingList.findIndex(x => x.pokingFriendName == this.pokingFriendName);
+      if (index != -1) {
+        this.pokingList[index]['numberOfPoking'] += 1;
       } else {
         this.pokingList.push({'pokingFriendName': this.pokingFriendName, 'numberOfPoking': 1});
       }
+      this.pokingList.sort(function(a, b) {
+          return b['numberOfPoking'] - a['numberOfPoking'];
+      });
+      this.pokeView();
+    },
+    betView: function() {
+        var sb = $('#succeedBet');
+        $("#succeedBet").nextAll().remove();
+        var fb = $('#failureBet');
+        $("#failureBet").nextAll().remove();
+        if (this.succeedBettingList){
+          for (var i = this.succeedBettingList.length - 1 ; i >= 0 ; i--) {
+            var newRow = $("<tr></tr>");
+            var newNumber = $(`<td>${'₩'+ this.numberWithCommas(this.succeedBettingList[i]['bettingAmount'])}</td>`);
+            var newName = $(`<td>${this.succeedBettingList[i]['bettingFriendName']}</td>`);
+            newRow.append([newNumber, newName]);
+            newRow.insertAfter(sb);
+          }
+        }
+        if (this.failureBettingList){
+          for (var i = this.failureBettingList.length - 1 ; i >= 0 ; i--) {
+            var newRow = $("<tr></tr>");
+            var newNumber = $(`<td>${'₩'+ this.numberWithCommas(this.failureBettingList[i]['bettingAmount'])}</td>`);
+            var newName = $(`<td>${this.failureBettingList[i]['bettingFriendName']}</td>`);
+            newRow.append([newNumber, newName]);
+            newRow.insertAfter(fb);
+          }
+        }
     },
     onBetting: function(e) {
-      if (this.bettingSide or this.bettingFriendName or this.bettingAmount is null) {
+      if (!(this.bettingSide && this.bettingFriendName && this.bettingAmount)) {
         alert("Please fill all the information, and choose which side to bet");
         return;
       }
-      if (this.bettingList includes this.bettingFriendName) {
-        this.bettingList[this.bettingFriendName] += this.bettingAmount;
-      } else {
-        this.bettingList.push({'bettingSide': this.bettingSide,
-                               'bettingFriendName': this.bettingFriendName,
-                               'bettingAmount': this.bettingAmount});
+      if (this.bettingSide == 'betOnSucceed'){
+        var index = this.succeedBettingList.findIndex(x => x.bettingFriendName == this.bettingFriendName);
+        if (index != this.succeedBettingList.length) {
+          this.succeedBettingList[index]['bettingAmount'] += this.bettingAmount;
+        } else {
+          this.succeedBettingList.push({'bettingFriendName': this.bettingFriendName,
+                                        'bettingAmount': this.bettingAmount});
+        }
       }
-    }
+      else {
+        var index = this.failureBettingList.findIndex(x => x.bettingFriendName == this.bettingFriendName);
+        if (index != this.bettingList.length) {
+          this.failureBettingList[index]['bettingAmount'] += this.bettingAmount;
+        } else {
+          this.failureBettingList.push({'bettingFriendName': this.bettingFriendName,
+                                       'bettingAmount': this.bettingAmount});
+        }
+      }
+      this.betView();
+    },
+    goalView: function(){
+      this.user_goals = [];
+      for (var i = 0 ; i < this.goals.length ; i++){
+        var value = (this.saved_money/this.goals[i]['money']).toFixed(2);
+        this.user_goals.push( value + " " + this.goals[i]['name']);
+      }
+    },
+    numberWithCommas: function(x){
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
   },
 });
